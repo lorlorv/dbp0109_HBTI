dc9354ad81fbc70f649a3321e1c905e19eb83bb5
Jeongminyooa <jmjmjmz732002@naver.com>
2021-11-15 16:50:12 +0900

    Feat : group Search Controller 구현

---
+++ src/main/java/controller/group/SearchGroupController.java
@@ -0,0 +1,31 @@
+package controller.group;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import controller.Controller;
+import model.Group;
+import model.service.UserManager;
+
+public class SearchGroupController implements Controller{
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+	
+    	// 로그인 여부 확인 추가
+    	UserManager userManager = UserManager.getInstance();	
+    	String keyword = request.getParameter("searchKeyword");
+    	String user_id = "jeongmin";
+    			//UserSessionUtils.getLoginUserId(request.getSession());
+    	
+    	// 쿼리를 위해 세팅
+    	keyword = "'" + keyword + "%'";
+    	System.out.println(keyword);
+    	
+    	int hbti_id = userManager.findHBTI(user_id);
+    	List<Group> searchList = userManager.searchGroupList(hbti_id, keyword);
+    	
+    	request.setAttribute("groupList", searchList);
+    	return "/group/list.jsp";
+    }
+}
