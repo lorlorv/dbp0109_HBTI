baff60a7857ae6e429899493ec70a97c5dd89fb8
Jeongminyooa <jmjmjmz732002@naver.com>
2021-12-15 14:58:54 +0900

    Fix : Todo mybatis 오류 수정

--- src/main/java/model/dao/mybatis/mapper/TodoMapper.java
+++ src/main/java/model/dao/mybatis/mapper/TodoMapper.java
@@ -8,8 +8,7 @@ import model.Todo;
 
 public interface TodoMapper {
    
-   public int addTodo(@Param("content") String content,
-         @Param("user_id") int user_id);
+   public int addTodo(Todo todo);
    
    public int updateTodo(@Param("content") String content,
          @Param("todo_id") int todo_id);
@@ -20,25 +19,25 @@ public interface TodoMapper {
    public int deleteTodo(int todo_id);
    
    public List<Todo> findDateTodoListByUserId(@Param("todo_date") java.sql.Date todo_date,
-         @Param("user_id") int user_id);
+         @Param("user_id") String user_id);
 
-   public List<Todo> findTodoListByUserId(int user_id);
+   public List<Todo> findTodoListByUserId(String user_id);
    
    public Todo findTodoByDate(@Param("todo_date") java.sql.Date todo_date,
          @Param("todo_id") int todo_id,
-         @Param("user_id") int user_id);
+         @Param("user_id") String user_id);
    
    public Todo findTodoByTodoID(
          @Param("todo_id") int todo_id,
-         @Param("user_id") int user_id);
+         @Param("user_id") String user_id);
    
    public List<Todo> findNotSelectTodoListByDate(@Param("todo_date") java.sql.Date todo_date,
          @Param("todo_id") int todo_id,
-         @Param("user_id") int user_id);
+         @Param("user_id") String user_id);
    
    public List<Todo> findNotSelectTodoList(
          @Param("todo_id") int todo_id,
-         @Param("user_id") int user_id);
+         @Param("user_id") String user_id);
    
    public int deleteUserAllTodo(String user_id);
    
